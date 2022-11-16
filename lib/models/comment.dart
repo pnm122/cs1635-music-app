ea1eb775f53f0e7a835b0bfbc748979152144b4d
Zach Heistand <zch5@pitt.edu>
2022-11-15 23:11:15 -0500

    fixed merge issues

--- lib/models/comment.dart
+++ lib/models/comment.dart
@@ -2,10 +2,10 @@ import 'package:test_app/models/user.dart';
 
 class Comment {
   final String content;
-  final String commenterUsername;
-  List<String> likedByUsers;
+  final User commenter;
+  List<User> likedBy;
   List<Comment> childComments;
 
-  Comment({required this.content, required this.commenterUsername, this.likedByUsers = const [], this.childComments = const []});
+  Comment({required this.content, required this.commenter, this.likedBy = const [], this.childComments = const []});
 
 }
