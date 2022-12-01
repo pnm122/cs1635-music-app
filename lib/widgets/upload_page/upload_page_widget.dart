5253370d7487df984b1f51ee9e713fab19f7dd61
Jiangjiang Song <jack@jiangjiangdembp.myfiosgateway.com>
2022-12-01 04:26:42 -0500

    Outlined buttons, icon & text now both clickable

--- lib/widgets/upload_page/upload_page_widget.dart
+++ lib/widgets/upload_page/upload_page_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:test_app/viewmodels/homepage/post_view_model.dart';
 import 'package:test_app/viewmodels/upload_page/song_record_view_model.dart';
 import 'package:test_app/viewmodels/upload_page/text_post_view_model.dart';
 
@@ -9,31 +8,75 @@ class UploadPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(onPressed: () {
+    Color foreground = Theme.of(context).colorScheme.primary;
+    Color background = Theme.of(context).colorScheme.background;
+    const double iconSize = 70;
+    const double fontSize = 20;
+    const double outlineWidth = 3;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton.icon(
+            onPressed: () {
               // TODO: Go to SongRecord
-            }, icon: const Icon(Icons.music_video)),
-            const Text('Song'),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(onPressed: () {
+              ChangeNotifierProvider(
+                create: (_) => SongRecordViewModel(),
+              );
+            },
+            icon: Icon(
+              Icons.music_video,
+              size: iconSize,
+              color: foreground,
+            ),
+            label: Text(
+              'Song',
+              style: TextStyle(
+                color: foreground,
+                fontSize: fontSize,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: background,
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              side: BorderSide(
+                width: outlineWidth,
+                color: foreground,
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          ElevatedButton.icon(
+            onPressed: () {
               // TODO: Go to TextPost
               ChangeNotifierProvider(
-                create: (_) => PostViewModel(),
+                create: (_) => TextPostViewModel(),
               );
-            }, icon: const Icon(Icons.chat_bubble_outline_sharp)),
-            const Text('Text'),
-          ],
-        ),
-      ],
+            },
+            icon: Icon(
+              Icons.chat_bubble_outline_sharp,
+              size: iconSize,
+              color: foreground,
+            ),
+            label: Text(
+              'Text',
+              style: TextStyle(
+                color: foreground,
+                fontSize: fontSize,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: background,
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              side: BorderSide(
+                width: outlineWidth,
+                color: foreground,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
