4d83cd0f1f3f833a51ef31b055e730b303098e0d
Jiangjiang (Jack) Song <jis110@pitt.edu>
2022-12-12 18:08:32 -0500

    Converted to floating buttons

--- lib/widgets/upload_page/song_record_widget.dart
+++ lib/widgets/upload_page/song_record_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
 
 import 'package:test_app/global_styles.dart';
 
@@ -19,76 +20,116 @@ class _SongRecordState extends State<SongRecordWidget> {
 
   @override
   void dispose() {
-    cameraController.dispose();
+    rearCamera.dispose();
+    frontCamera.dispose();
     super.dispose();
   }
 
   late List<CameraDescription> cameras;
-  late CameraController cameraController;
+  late CameraController rearCamera;
+  late CameraController frontCamera;
   void startCam() async {
     cameras = await availableCameras();
-    cameraController = CameraController(
+
+    rearCamera = CameraController(
       cameras.first,
       ResolutionPreset.max,
       enableAudio: true,
     );
-    await cameraController.initialize().then((value) {
-      if (!mounted) {
-        return;
-      }
+    await rearCamera.initialize().then((value) {
+      if (!mounted) return;
       setState(() {});
     }).catchError((e) {
       return; // Error is caught in the code below
     });
+    rearCamera.lockCaptureOrientation(DeviceOrientation.portraitUp);
+
+    frontCamera = CameraController(
+      cameras.last,
+      ResolutionPreset.max,
+      enableAudio: true,
+    );
+    await frontCamera.initialize().then((value) {
+      if (!mounted) return;
+      setState(() {});
+    }).catchError((e) {
+      return;
+    });
+    // For some reason, the webcam is in the wrong orientation by
+    frontCamera.lockCaptureOrientation(DeviceOrientation.landscapeRight);
   }
 
   @override
   Widget build(BuildContext context) {
     // Guard camera from the big-angry-red-screen
-    if (!cameraController.value.isInitialized) {
-      return const Scaffold(
-        body: Text("Something is wrong with Camera"),
+    if (!rearCamera.value.isInitialized || !frontCamera.value.isInitialized) {
+      return const Center(
+        child: Icon(
+          Icons.refresh,
+          size: 200,
+        ),
       );
     }
 
     return Stack(
       children: [
-        Center(child: CameraPreview(cameraController)),
-        Scaffold(
-          appBar: AppBar(
-            shadowColor: Colors.transparent,
+        Center(
+          child: CameraPreview(rearCamera),
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          child: FloatingActionButton.large(
+            foregroundColor: Colors.white,
             backgroundColor: Colors.transparent,
+            elevation: 0,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              Icons.arrow_back,
+              size: navBarIconSize,
+            ),
           ),
-          bottomNavigationBar: BottomNavigationBar(
+        ),
+        Container(
+          alignment: Alignment.bottomLeft,
+          child: FloatingActionButton.large(
+            foregroundColor: Colors.white,
             backgroundColor: Colors.transparent,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.insert_photo,
-                  size: navBarIconSize,
-                ),
-                label: 'Select Video',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.fiber_manual_record_outlined,
-                  size: 100,
-                ),
-                label: 'Record',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.flip_camera_ios,
-                  size: navBarIconSize,
-                ),
-                label: 'Flip Camera',
-              ),
-            ],
+            elevation: 0,
+            onPressed: () {}, // TODO:
+            child: const Icon(
+              Icons.image,
+              size: navBarIconSize,
+            ),
           ),
-          backgroundColor: Colors.transparent,
-        )
+        ),
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: FloatingActionButton.large(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            onPressed: () {}, // TODO:
+            child: const Icon(
+              Icons.fiber_manual_record_outlined,
+              size: 90,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.bottomRight,
+          child: FloatingActionButton.large(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            onPressed: () {}, // TODO:
+            child: const Icon(
+              Icons.flip_camera_ios_rounded,
+              size: navBarIconSize,
+            ),
+          ),
+        ),
       ],
     );
   }
