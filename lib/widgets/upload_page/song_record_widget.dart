5ce549edf696efd491621e35b94c28826b32b09f
Jiangjiang (Jack) Song <jis110@pitt.edu>
2022-12-13 09:24:36 -0500

    Flip camera done

--- lib/widgets/upload_page/song_record_widget.dart
+++ lib/widgets/upload_page/song_record_widget.dart
@@ -21,49 +21,32 @@ class _SongRecordState extends State<SongRecordWidget> {
 
   @override
   void dispose() {
-    rearCamera.dispose();
-    frontCamera.dispose();
+    camController.dispose();
     super.dispose();
   }
 
   late List<CameraDescription> cameras;
-  late CameraController rearCamera;
-  late CameraController frontCamera;
+  late CameraController camController;
   void startCam() async {
     cameras = await availableCameras();
 
-    rearCamera = CameraController(
+    camController = CameraController(
       cameras.first,
       ResolutionPreset.max,
-      enableAudio: true,
     );
-    await rearCamera.initialize().then((value) {
+    await camController.initialize().then((_) {
       if (!mounted) return;
       setState(() {});
     }).catchError((e) {
-      return; // Error is caught in the code below
+      print(e); // Error is caught in the code below
     });
-    rearCamera.lockCaptureOrientation(DeviceOrientation.portraitUp);
-
-    frontCamera = CameraController(
-      cameras.last,
-      ResolutionPreset.max,
-      enableAudio: true,
-    );
-    await frontCamera.initialize().then((value) {
-      if (!mounted) return;
-      setState(() {});
-    }).catchError((e) {
-      return;
-    });
-    // For some reason, the webcam is in the wrong orientation by
-    frontCamera.lockCaptureOrientation(DeviceOrientation.landscapeRight);
   }
 
+  bool isFrontCam = false;
   @override
   Widget build(BuildContext context) {
     // Guard camera from the big-angry-red-screen
-    if (!rearCamera.value.isInitialized || !frontCamera.value.isInitialized) {
+    if (!camController.value.isInitialized) {
       return const Center(
         child: Icon(
           Icons.refresh,
@@ -72,70 +55,91 @@ class _SongRecordState extends State<SongRecordWidget> {
       );
     }
 
-    return Stack(
-      children: [
-        Center(
-          child: CameraPreview(rearCamera),
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          child: FloatingActionButton.large(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Icon(
-              Icons.arrow_back,
-              size: navBarIconSize,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: CameraPreview(camController),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: FloatingActionButton.large(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              heroTag: "Go Back",
+              child: const Icon(
+                Icons.arrow_back,
+                size: navBarIconSize,
+              ),
             ),
           ),
-        ),
-        Container(
-          alignment: Alignment.bottomLeft,
-          child: FloatingActionButton.large(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            onPressed: () {}, // TODO:
-            child: const Icon(
-              Icons.image,
-              size: navBarIconSize,
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: FloatingActionButton.large(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              onPressed: () {}, // TODO:
+              heroTag: "From Video Library",
+              child: const Icon(
+                Icons.image,
+                size: navBarIconSize,
+              ),
             ),
           ),
-        ),
-        Container(
-          alignment: Alignment.bottomCenter,
-          child: FloatingActionButton.large(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const SongEditWidget(),
-              ));
-            }, // TODO:
-            child: const Icon(
-              Icons.fiber_manual_record_outlined,
-              size: 90,
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: FloatingActionButton.large(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const SongEditWidget(),
+                ));
+              }, // TODO:
+              heroTag: "Record",
+              child: const Icon(
+                Icons.fiber_manual_record_outlined,
+                size: 90,
+              ),
             ),
           ),
-        ),
-        Container(
-          alignment: Alignment.bottomRight,
-          child: FloatingActionButton.large(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            onPressed: () {}, // TODO:
-            child: const Icon(
-              Icons.flip_camera_ios_rounded,
-              size: navBarIconSize,
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton.large(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              onPressed: () async {
+                setState(() {
+                  isFrontCam = !isFrontCam;
+                });
+                int camPos = isFrontCam ? 0 : 1;
+                camController = CameraController(
+                  cameras[camPos],
+                  ResolutionPreset.max,
+                );
+                await camController.initialize().then((_) {
+                  if (!mounted) return;
+                  setState(() {});
+                }).catchError((e) {
+                  print(e);
+                });
+              },
+              heroTag: "Flip Camera",
+              child: const Icon(
+                Icons.flip_camera_ios_rounded,
+                size: navBarIconSize,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
